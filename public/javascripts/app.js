61f1c58dc3f63634725d68eb9bc3150ed4042157
John <john.robert.larson@gmail.com>
2016-03-21 22:47:25 +0000

    removed default comment list

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,13 +3,7 @@ angular.module('comment', [])
 .controller('MainCtrl', [
 	'$scope', '$http',
   	function($scope, $http){
-		$scope.comments = [
-			{title:'Comment 1', upvotes:5},
-			{title:'Comment 2', upvotes:6},
-			{title:'Comment 3', upvotes:1},
-			{title:'Comment 4', upvotes:4},
-			{title:'Comment 5', upvotes:3}
-		];
+		$scope.comments = [];
 		$scope.addComment = function() {
 			if($scope.formContent === '') { return; }
 			$scope.create({
